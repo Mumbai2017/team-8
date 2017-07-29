3187ff2b45b6b67c00703205d4491b3bd6a857d6
jayeshnagpal <nagpaljayesh57@gmail.com>
2017-07-30 01:30:34 +0530

    Add volunteer api

--- web_api/addVolunteer.php
+++ web_api/addVolunteer.php
@@ -1,24 +1,16 @@
 <?php
 include'config.php';
-/*
+
+
 $email=$_POST['email'];
 $first = $_POST['fname'];
 $last = $_POST['lname'];
 $dob=$_POST['dob'];
 $credits=0;
 $joindate=$_POST['joindate'];
-*/
-$query = "Select * from volunteer";
-$statement = $con->prepare($query);
 
-$statement->execute();
-$result = $statement->get_result();
-$row = $result->fetch_assoc();
-if ($result->num_rows >= 1) {
-    echo "true";
-}
-else {
-    echo "false";
-}
-$statement->close();
+$stmt = $con->prepare("INSERT INTO volunteer (fname, lname, dob, email, joindate, credits) VALUES (?, ?, ?, ? , ?)");
+$stmt->bind_param($first, $last, $dob, $email, $joindate, $credits);
+$stmt->execute();
+$stmt->close();
 ?>
