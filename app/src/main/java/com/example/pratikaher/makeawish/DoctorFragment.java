c797baf47f487051778d1f80ee5ea3c192d2daf4
jayeshnagpal <nagpaljayesh57@gmail.com>
2017-07-30 04:27:32 +0530

    api connect

--- app/src/main/java/com/example/pratikaher/makeawish/DoctorFragment.java
+++ app/src/main/java/com/example/pratikaher/makeawish/DoctorFragment.java
@@ -6,6 +6,11 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import java.io.UnsupportedEncodingException;
+
+import static android.R.id.content;
 
 /**
  * Created by pratikaher on 29/07/17.
@@ -17,9 +22,101 @@ public class DoctorFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
 
-//        layoutinflater = inflater.inflate(R.layout.doctor_history, container, false);
-
         layoutinflater = inflater.inflate(R.layout.doctor_home, container, false);
+
+        Button saveme=(Button) layoutinflater.findViewById(R.id.submit_btn);
+
+        saveme.setOnClickListener(new Button.OnClickListener(){
+
+            public void onClick(View v)
+            {
+                try{
+
+                    // CALL GetText method to make post method call
+                    GetText();
+                }
+                catch (Exception e) {
+                }
+            }
+        });
+    }
+
+    // Create GetText Metod
+    public  void  GetText()  throws UnsupportedEncodingException
+    {
+        // Get user defined values
+        String name = layoutinflater.findViewById(R.id.name).toString();
+        String pid = layoutinflater.findViewById(R.id.pid).toString();
+        String pname = layoutinflater.findViewById(R.id.pname).toString();
+        String docname = layoutinflater.findViewById(R.id.docname).toString();
+        String ill = layoutinflater.findViewById(R.id.ill).toString();
+        // Create data variable for sent values to server
+
+        String data = URLEncoder.encode("name", "UTF-8")
+                + "=" + URLEncoder.encode(Name, "UTF-8");
+
+        data += "&" + URLEncoder.encode("email", "UTF-8") + "="
+                + URLEncoder.encode(Email, "UTF-8");
+
+        data += "&" + URLEncoder.encode("user", "UTF-8")
+                + "=" + URLEncoder.encode(Login, "UTF-8");
+
+        data += "&" + URLEncoder.encode("pass", "UTF-8")
+                + "=" + URLEncoder.encode(Pass, "UTF-8");
+
+        String text = "";
+        BufferedReader reader=null;
+
+        // Send data
+        try
+        {
+
+            // Defined URL  where to send data
+            URL url = new URL("http://androidexample.com/media/webservice/httppost.php");
+
+            // Send POST data request
+
+            URLConnection conn = url.openConnection();
+            conn.setDoOutput(true);
+            OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+            wr.write( data );
+            wr.flush();
+
+            // Get the server response
+
+            reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            StringBuilder sb = new StringBuilder();
+            String line = null;
+
+            // Read Server Response
+            while((line = reader.readLine()) != null)
+            {
+                // Append server response in string
+                sb.append(line + "\n");
+            }
+
+
+            text = sb.toString();
+        }
+        catch(Exception ex)
+        {
+
+        }
+        finally
+        {
+            try
+            {
+
+                reader.close();
+            }
+
+            catch(Exception ex) {}
+        }
+
+        // Show response on activity
+        content.setText( text  );
+
+    }
 
 
         return layoutinflater;
