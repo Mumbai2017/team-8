3dc6e540020fe068bf0492e6b113608dfb9edbfe
jayeshnagpal <nagpaljayesh57@gmail.com>
2017-07-30 02:05:25 +0530

    Removed duplication

--- web_api/add_child.php
+++ web_api/add_child.php
@@ -11,10 +11,21 @@ $wish_id = $_POST['wish_id'];
 
 include'config.php';
 
-$stmt = $con->prepare("INSERT INTO child (parent_id, child_name, dob, doctor_name, doctor_id, volunteer_id, illness, wish_id ) VALUES (?, ?, ?, ? ,?,?,?,?,?)");
-$stmt->bind_param($parent_id,$child_name,$dob,$parent_name,$doctor_name,$doctor_id,$volunteer_id, $illness,$wish_id );
-
-$stmt->execute();
-$stmt->close();
+$query = "Select * from child where parent_id = ? and illness  = ?";
+$statement = $con->prepare($query);
+$statement->bind_param($parent_id,$illness);
+$statement->execute();
+$result = $statement->get_result();
+$row = $result->fetch_assoc();
+if ($result->num_rows > 0){
+    echo "Application Invalid";
+}
+else{
+    $stmt = $con->prepare("INSERT INTO child (parent_id, child_name, dob, doctor_name, doctor_id, volunteer_id, illness, wish_id ) VALUES (?, ?, ?, ? ,?,?,?,?,?)");
+    $stmt->bind_param($parent_id,$child_name,$dob,$parent_name,$doctor_name,$doctor_id,$volunteer_id, $illness,$wish_id );
+    $stmt->execute();
+    $stmt->close();
+}
+$statement->close();
 
 ?>
