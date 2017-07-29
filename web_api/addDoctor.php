2c832e7548e8d65e1f0aa1c8f84dc5c56a8b6e5d
jayeshnagpal <nagpaljayesh57@gmail.com>
2017-07-30 01:54:08 +0530

    doctor_add api

--- web_api/addDoctor.php
+++ web_api/addDoctor.php
@@ -5,7 +5,12 @@ $first = $_POST['fname'];
 $doctor_id = $_POST['doctor_id'];
 $email=$_POST['email'];
 $pass=$_POST['password'];
+$hospital_name = $_POST['hospital_name'];
 
 
-$statement->close();
+$stmt = $con->prepare("INSERT INTO doctor (fname, doctor_id, email, password, hospital_name) VALUES (?, ?, ?,? ,?)");
+$stmt->bind_param($first, $doctor_id,$email, $pass, $hospital_name);
+$stmt->execute();
+$stmt->close();
+$stmt->close();
 ?>
