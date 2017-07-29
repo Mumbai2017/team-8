32526699fdbe757c55a19cebf999612eb77d34fc
jayeshnagpal <nagpaljayesh57@gmail.com>
2017-07-30 01:48:43 +0530

    child_add api

--- web_api/addDoctor.php
+++ web_api/addDoctor.php
@@ -2,30 +2,10 @@
 
 session_start();
 $first = $_POST['fname'];
-$last = $_POST['lname'];
+$doctor_id = $_POST['doctor_id'];
 $email=$_POST['email'];
 $pass=$_POST['password'];
-$coll = 'cfg8.doctor';
 
-include('config.php');
-$filter = ['email' => $email, 'password' => $pass ];
-$query = new MongoDB\Driver\Query($filter);
-$res = $mng->executeQuery($coll, $query);
-$rows = current($res->toArray());
 
-if (!empty($rows))
-    echo "false";
-else
-{
-    $bulk = new MongoDB\Driver\BulkWrite;
-    $ins = [
-        'name' => array('first' => $first,'last'=>$last),
-        'email' => $email,
-        'password' => $pass,
-    ];
-    $bulk->insert($ins);
-    $mng->executeBulkWrite($coll, $bulk);
-    echo "true";
-}
 $statement->close();
 ?>
