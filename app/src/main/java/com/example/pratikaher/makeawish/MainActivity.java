6bd9cd593848610ae586bc8425392890daa374fd
siddharth <sid.bagdi@yahoo.com>
2017-07-29 20:31:39 +0530

    Added XML for Volunteer and Doctor profile

--- app/src/main/java/com/example/pratikaher/makeawish/MainActivity.java
+++ app/src/main/java/com/example/pratikaher/makeawish/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        BottomNavigationView bottomNavigationView = (BottomNavigationView)
+/*        BottomNavigationView bottomNavigationView = (BottomNavigationView)
                 findViewById(R.id.bottom_navigation);
 
         bottomNavigationView.setOnNavigationItemSelectedListener(
@@ -30,6 +30,6 @@ public class MainActivity extends AppCompatActivity {
                         }
                         return true;
                     }
-                });
+                });*/
     }
 }
