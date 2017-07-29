c5156d484521f2a7b12a800bce79cc0743eaf093
pratikaher <pratikaher88@gmail.com>
2017-07-29 18:22:01 +0530

    message

--- app/src/main/java/com/example/pratikaher/makeawish/MainActivity.java
+++ app/src/main/java/com/example/pratikaher/makeawish/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.pratikaher.makeawish;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)
+                findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.doctor_home:
+
+                            case R.id.doctor_history:
+
+                            case R.id.doctor_profile:
+
+                        }
+                        return true;
+                    }
+                });
     }
 }
