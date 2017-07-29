8ebb5510124cab5945fe4f8970ea859ec372e6e1
jayeshnagpal <nagpaljayesh57@gmail.com>
2017-07-30 01:14:08 +0530

    api update

--- web_api/addVolunteer.php
+++ web_api/addVolunteer.php
@@ -1,35 +1,24 @@
 <?php
-//$name=$_POST['name'];
+include'config.php';
+/*
 $email=$_POST['email'];
-//$username=$_POST['username'];
 $first = $_POST['fname'];
 $last = $_POST['lname'];
 $dob=$_POST['dob'];
-$password=$_POST['password'];
 $credits=0;
 $joindate=$_POST['joindate'];
-include'config.php';
-$filter = ['email' => $email,];
-$query = new MongoDB\Driver\Query($filter);
-$coll = 'cgf8.volunteer';
-$res = $mng->executeQuery($coll, $query);
-$rows = current($res->toArray());
-if (!empty($rows))
-    echo"false";
-else
-{
-    $bulk = new MongoDB\Driver\BulkWrite;
-    $ins = [
-        'name' => array('first' => $first,
-        'last'=>$last),
-        'email' => $email,
-        'dob'=> $dob,
-        'credits'=>$credits,
-        'joindate'=>$joindate,
-        'password' => $password,
-    ];
-    $bulk->insert($ins);
-    $mng->executeBulkWrite($coll, $bulk);
-    echo"true";
+*/
+$query = "Select * from volunteer";
+$statement = $con->prepare($query);
+
+$statement->execute();
+$result = $statement->get_result();
+$row = $result->fetch_assoc();
+if ($result->num_rows >= 1) {
+    echo "true";
 }
+else {
+    echo "false";
+}
+$statement->close();
 ?>
